7450a71e5a72f88611882b616a8868e86b97f820
cenkayxkucur <20060398@stu.omu.edu.tr>
2025-08-03 22:19:25 +0300

    Ana sayfadaki Slider'a verilerin database den cekilmesi icin Slider Api hazirlandi, Consume ile devam edilecek

---
+++ SignalR.DataAccessLayer/Migrations/20250803185208_mig_add_slider.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SignalR.DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_add_slider : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Sliders",
+                columns: table => new
+                {
+                    SliderID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SliderTitle1 = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SliderTitle2 = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SliderTitle3 = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SliderDescription1 = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SliderDescription2 = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SliderDescription3 = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sliders", x => x.SliderID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Sliders");
+        }
+    }
+}
---
+++ SignalREntityLayer/Entities/Slider.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.EntityLayer.Entities
+{
+    public class Slider
+    {
+        public int SliderID { get; set; }
+        public string SliderTitle1 { get; set; }
+        public string SliderTitle2 { get; set; }
+        public string SliderTitle3 { get; set; }
+        public string SliderDescription1 { get; set; }
+        public string SliderDescription2 { get; set; }
+        public string SliderDescription3 { get; set; }
+
+
+    }
+}
