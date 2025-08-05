218dede3ede0012a280bc16cec2c7b0b4ac86c32
cenkayxkucur <20060398@stu.omu.edu.tr>
2025-08-05 21:27:43 +0300

    Slider Api nin Consume edilmesini sagladik

---
+++ SignalRWebUI/Dtos/SliderDtos/ResultSliderDto.cs
@@ -0,0 +1,14 @@
+﻿namespace SignalRWebUI.Dtos.SliderDtos
+{
+    public class ResultSliderDto
+    {
+        public int SliderId { get; set; }
+        public string SliderTitle1 { get; set; }
+        public string SliderTitle2 { get; set; }
+        public string SliderTitle3 { get; set; }
+        public string SliderDescription1 { get; set; }
+        public string SliderDescription2 { get; set; }
+        public string SliderDescription3 { get; set; }
+
+    }
+}
