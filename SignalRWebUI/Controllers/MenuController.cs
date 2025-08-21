5fd4161b378868890ee0d0dfe57f2438b2842c2d
cenkayxkucur <20060398@stu.omu.edu.tr>
2025-08-22 01:46:41 +0300

    Urunlerin menuye eklenmesi

--- SignalRWebUI/Controllers/MenuController.cs
+++ SignalRWebUI/Controllers/MenuController.cs
@@ -1,12 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalRWebUI.Dtos.ProductDtos;
 
 namespace SignalRWebUI.Controllers
 {
     public class MenuController : Controller
     {
-        public IActionResult Index()
+        public readonly IHttpClientFactory _httpClientFactory;
+
+        public MenuController(IHttpClientFactory httpClientFactory)
         {
-            return View();
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7256/api/Product");
+
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultProductDto>>(jsonData);
+            return View(values);
         }
     }
 }
