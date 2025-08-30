2d1ff2655b76cc406a9836f92316f96f9c9ca5b3
cenkayxkucur <20060398@stu.omu.edu.tr>
2025-08-30 17:26:57 +0300

    Sepet ekrani icin tasarim olusturuldu,WebUI kismina controller,dto ve view eklendi

---
+++ SignalRWebUI/Dtos/BasketDtos/ResultBasketDto.cs
@@ -0,0 +1,12 @@
+﻿namespace SignalRWebUI.Dtos.BasketDtos
+{
+    public class ResultBasketDto
+    {
+        public int BasketID { get; set; }
+        public decimal ProductPrice { get; set; }
+        public decimal BasketProductCount { get; set; }
+        public decimal BasketProductTotalPrice { get; set; }
+        public int ProductID { get; set; }
+        public int MenuTableID { get; set; }
+    }
+}
